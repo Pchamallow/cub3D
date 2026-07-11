192803e0177a6b20a0337e7c14f0f1fb6a5f9d16
Nicolas Baudoin WSL <baudoin.nicolasg@gmail.com>
2026-07-11 08:47:57 +0200

    feat(free): Create directory free with free.c to have free utilities - add free_all managing all the free - add free_images in case we have it - add free texture if we allocate them - add free map, can be removed

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: nbaudoin <nbaudoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 13:50:07 by pswirgie          #+#    #+#             */
-/*   Updated: 2026/07/11 01:51:49 by nbaudoin         ###   ########.fr       */
+/*   Updated: 2026/07/11 08:46:17 by nbaudoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,5 +86,9 @@ int		args_not_valid(int ac, char **av);
 void	display_error(char *error_msg);
 void	display_perror(void);
 int		mlx_error(void);
+
+// free
+
+void	free_all(t_data *data);
 
 #endif
