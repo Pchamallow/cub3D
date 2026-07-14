8566e85bb1f4b5e45e360be47faa31c91178d8e0
Pauline Swirgiel <pswirgie@1E2.42angouleme.fr>
2026-07-14 15:14:50 +0200

    feat(raytracing): wip

--- srcs/math/raycasting.c
+++ srcs/math/raycasting.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   raytracing.c                                       :+:      :+:    :+:   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pswirgie <pswirgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 12:41:36 by pswirgie          #+#    #+#             */
-/*   Updated: 2026/07/14 14:37:13 by pswirgie         ###   ########.fr       */
+/*   Updated: 2026/07/14 15:00:15 by pswirgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 #include "../../lib/minilibx-linux/mlx.h"
-
-
+#include <stdint.h>
 
 // verLine(x, drawStart, drawEnd, color);
 
@@ -46,9 +45,31 @@ static void	modifiy_render(t_data *data, t_render *render)
 	}
 }
 
+int	raycasting(t_data *data)
+{
+	double posX = 22.0, posY = 11.5;  //x and y start position
+	double dirX = -1.0, dirY = 0.0; //initial direction vector
+	double planeX = 0.0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+	double time = 0; //time of current frame
+	double oldTime = 0; //time of previous frame
+
+	uint32_t buffer[data->height][data->width]; // y-coordinate first because it works per scanline
+// 	int *texture[8];
+// 	int i = 0;
+// 	while(i < 8)
+// 	{
+// 		texture[i].resize(data->width * data->height);
+// 		i++;
+// 	}
+	
+
+}
+
 int	render(t_data *data)
 {
 	t_render	*render = &data->render;
+	raycasting();
 	modifiy_render(data, render);
 	mlx_put_image_to_window(data->mlx, data->win, data->render.image, 0, 0);
 	return (0);
