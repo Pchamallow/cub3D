1a2e9267ba241eecaa1f5a176d6aceeeb2efe11c
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-07-15 10:13:31 +0200

    fix(split): remove unessessary var and simplify  is valid number and get rgb

--- srcs/utils/split.c
+++ srcs/utils/split.c
@@ -21,23 +21,26 @@ static void	print_invalid_rgb(void)
 
 static int	is_invalid_numbers(char *file, int *j, int *nb)
 {
+	int	len;
+
+	len = 0;
 	while (file[*j] && ft_isdigit(file[*j]))
+	{
+		len++;
 		*j += 1;
+	}
 	*j += skip_spaces(&file[*j]);
-	if (*nb < 3 && file[*j] != ',')
+	if (len > 3 || (*nb < 3 && (file[*j] != ','
+				|| !ft_isdigit(file[*j + 1 + skip_spaces(&file[*j + 1])]))))
 	{
 		print_invalid_rgb();
 		return (1);
 	}
-	else if (*nb == 3)
+	if (*nb == 3 && file[*j] && file[*j] != '\n')
 	{
-		*j += skip_spaces(&file[*j]);
-		if (file[*j] && file[*j] != '\n')
-		{
-			ft_display_error("Content after element RGB"
-				" detected, usage: [e] [R], [G], [B]");
-			return (1);
-		}
+		ft_display_error("Content after element RGB"
+			" detected, usage: [e] [R], [G], [B]");
+		return (1);
 	}
 	return (0);
 }
@@ -77,27 +80,22 @@ static char	**get_rgb(char *file, int *j)
 {
 	char	**rgb;
 
-	rgb = NULL;
-	if (!file[*j] || (file[*j] && file[*j] != ' '))
+	if (file[*j] != ' ')
 	{
 		print_invalid_rgb();
 		return (NULL);
 	}
-	if (file[*j] && file[*j] == ' ')
+	*j += skip_spaces(&file[*j]);
+	if (!file[*j] || !ft_isdigit(file[*j]))
 	{
-		*j += skip_spaces(&file[*j]);
-		if (!file[*j] || !ft_isdigit(file[*j]))
-		{
-			print_invalid_rgb();
-			return (NULL);
-		}
-		if (is_invalid_rgb(file, *j))
-			return (NULL);
-		rgb = ft_split(&file[*j], ',');
-		if (!rgb)
-			ft_display_error("Split RGB - allocation memory failed");
-		return (rgb);
+		print_invalid_rgb();
+		return (NULL);
 	}
+	if (is_invalid_rgb(file, *j))
+		return (NULL);
+	rgb = ft_split(&file[*j], ',');
+	if (!rgb)
+		ft_display_error("Split RGB - allocation memory failed");
 	return (rgb);
 }
 
@@ -108,23 +106,21 @@ static char	**get_rgb(char *file, int *j)
 char	**ft_split_rgb(t_data *data, char *e)
 {
 	char	**file;
-	char	**rgb;
 	int		i;
 	int		j;
-	int		nb;
 
 	file = data->map.full_file;
 	i = 0;
-	nb = 0;
 	while (file[i])
 	{
 		j = 1;
 		if (!ft_strncmp(file[i], e, 1))
-		{
-			rgb = get_rgb(file[i], &j);
-			return (rgb);
-		}
+			return (get_rgb(file[i], &j));
 		i++;
 	}
+	ft_printf_fd(2, RED "Error\n");
+	ft_printf_fd(2, "Missing element: %s,"
+		" usage: [%s] [R], [G], [B]", e, e);
+	ft_printf_fd(2, "\n" RESET);
 	return (NULL);
 }
