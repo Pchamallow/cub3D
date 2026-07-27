bace584cb19a547dcd00d7f3d9fe03a1fbd025e8
Pauline Swirgiel <pswirgie@1E2.42angouleme.fr>
2026-07-27 14:04:33 +0200

    feat(player_start_position): valid for all directions

--- srcs/math/init_direction.c
+++ srcs/math/init_direction.c
@@ -6,7 +6,7 @@
 /*   By: pswirgie <pswirgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 10:49:30 by pswirgie          #+#    #+#             */
-/*   Updated: 2026/07/27 14:01:15 by pswirgie         ###   ########.fr       */
+/*   Updated: 2026/07/27 14:04:07 by pswirgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	init_direction(t_data *data)
 	if (data->player.direction == 'N')
 		dirp = PI;
 	else if (data->player.direction == 'S')
-		dirp = 0.5 * PI;
-	else if (data->player.direction == 'W')
 		dirp = 0;
-	else if (data->player.direction == 'E')
+	else if (data->player.direction == 'W')
 		dirp = 1.5 * PI;
+	else if (data->player.direction == 'E')
+		dirp = 0.5 * PI;
 	data->player.dir_x = cos(dirp);
 	data->player.dir_y = sin(dirp);
 	data->player.dirp = dirp;
