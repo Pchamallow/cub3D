a4b1a3a9c3ceb0477175e10f679371a45fd8c98c
Pchamallow <pswirgie@DESKTOP-V2GHCI5.localdomain>
2026-08-05 09:39:22 +0200

    fix(map_parsing): ensure all 0 are surrounded by 1
    - add is_surrounded() and ft_index_find() at check_walls.c

--- srcs/parser/map/check_walls.c
+++ srcs/parser/map/check_walls.c
@@ -6,7 +6,7 @@
 /*   By: pswirgie <pswirgie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 16:14:32 by pswirgie          #+#    #+#             */
-/*   Updated: 2026/07/14 10:57:56 by pswirgie         ###   ########.fr       */
+/*   Updated: 2026/08/05 09:38:18 by pswirgie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@
 void	find_path(t_data *data, int y, int x)
 {
 	int	lines;
-	int	columns;
 
 	lines = data->map.lines + 1;
-	columns = data->map.columns + 1;
-	if (y < 0 || x < 0 || y > lines || x >= columns
+	if (y < 0 || x < 0 || y > lines || x >= data->map.columns
 		|| data->map.maze[y][x] == '1'
-		|| data->map.maze[y][x] == 'G')
+		|| data->map.maze[y][x] == 'G'
+		|| data->map.maze[y][x] == '\n')
 		return ;
 	else
 	{
@@ -39,6 +38,44 @@ void	find_path(t_data *data, int y, int x)
 	return ;
 }
 
+static void	ft_index_find(char *str, char c, int *index)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	if (i >= ft_strlen(str))
+	{
+		*index = -1;
+		return ;
+	}
+	*index = i;
+}
+
+static int	is_surrounded(t_data *data)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	x = 0;
+	while (data->map.maze[y])
+	{
+		if (data->map.wall_missing)
+			return (1);
+		ft_index_find(data->map.maze[y], '0', &x);
+		if (x != -1)
+		{
+			find_path(data, y, x);
+			y = 0;
+		}
+		else
+			y++;
+	}
+	return (0);
+}
+
 int	check_walls(t_data *data)
 {
 	int	y;
@@ -47,7 +84,7 @@ int	check_walls(t_data *data)
 	y = data->player.y_start_p;
 	x = data->player.x_start_p;
 	find_path(data, y, x);
-	if (data->map.wall_missing)
+	if (is_surrounded(data))
 	{
 		ft_display_error("At least one wall is missing");
 		return (1);
